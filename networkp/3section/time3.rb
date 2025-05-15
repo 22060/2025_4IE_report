9cebbcb974f055a9b45c942db749cd916da5763e
22060 <108120783+22060@users.noreply.github.com>
2025-05-16 08:55:12 +0900

    auto-commit

--- networkp/3section/time3.rb
+++ networkp/3section/time3.rb
@@ -24,8 +24,43 @@ if ARGV[1] !~ /^[0-9]+$/
     print "arg2 must be number\n"
     exit
 end
-a = Time.local(ARGV[0][0..3], ARGV[0][4..5], ARGV[0][6..7])
-b = Time.local(ARGV[1][0..3], ARGV[1][4..5], ARGV[1][6..7])
+if(ARGV[0][0..3].to_i < 1970)
+    print "usage: ruby time3.rb YYYYMMDD YYYYMMDD\n"
+    print "arg1 must be more than 19700101\n"
+    exit
+end
+if(ARGV[1][0..3].to_i < 1970)
+    print "usage: ruby time3.rb YYYYMMDD YYYYMMDD\n"
+    print "arg2 must be more than 19700101\n"
+    exit
+end
+if(ARGV[0][4..5].to_i < 1 || ARGV[0][4..5].to_i > 12)
+    print "usage: ruby time3.rb YYYYMMDD YYYYMMDD\n"
+    print "arg1 month must be 1-12\n"
+    exit
+end
+if(ARGV[1][4..5].to_i < 1 || ARGV[1][4..5].to_i > 12)
+    print "usage: ruby time3.rb YYYYMMDD YYYYMMDD\n"
+    print "arg2 month must be 1-12\n"
+    exit
+end
+if(ARGV[0][6..7].to_i < 1 || ARGV[0][6..7].to_i > 31)
+    print "usage: ruby time3.rb YYYYMMDD YYYYMMDD\n"
+    print "arg1 day must be 1-31\n"
+    exit
+end
+if(ARGV[1][6..7].to_i < 1 || ARGV[1][6..7].to_i > 31)
+    print "usage: ruby time3.rb YYYYMMDD YYYYMMDD\n"
+    print "arg2 day must be 1-31\n"
+    exit
+end
+begin 
+    a = Time.local(ARGV[0][0..3], ARGV[0][4..5], ARGV[0][6..7])
+    b = Time.local(ARGV[1][0..3], ARGV[1][4..5], ARGV[1][6..7])
+rescue => e # 例外オブジェクトを代入した変数。
+    print "usage: ruby time3.rb YYYYMMDD YYYYMMDD\n"
+    exit
+end
 if(a.to_i < 0)
     print "usage: ruby time3.rb YYYYMMDD YYYYMMDD\n"
     print "arg1 must be more than 19700101\n"
