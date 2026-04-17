68c50b615369239147f8ccc471ac2fe19ecf266f
22060 <108120783+22060@users.noreply.github.com>
2026-04-17 20:04:31 +0900

    a

--- src/code/ESC/main.cpp
+++ src/code/ESC/main.cpp
@@ -1,84 +1,80 @@
-#ifdef ESC1
-// #include <SimpleFOC.h>
-
-// BLDCMotor motor = BLDCMotor(7);
-// BLDCDriver6PWM driver = BLDCDriver6PWM(A_PHASE_UH, A_PHASE_UL, A_PHASE_VH, A_PHASE_VL, A_PHASE_WH, A_PHASE_WL);
-// LowsideCurrentSense currentSense = LowsideCurrentSense(0.003f, -64.0f / 7.0f, A_OP1_OUT, A_OP2_OUT, A_OP3_OUT);
-
-// Encoder encoder = Encoder(A_HALL1, A_HALL2, 2048, A_HALL3);
-
-// void doA() { encoder.handleA(); }
-// void doB() { encoder.handleB(); }
-// void doIndex() { encoder.handleIndex(); }
-
-// Commander command = Commander(Serial);
-// void doTarget(char *cmd) { command.motion(&motor, cmd); }
-
 #include <SimpleFOC.h>
 
+// ===== モーター・ドライバ =====
 BLDCMotor motor = BLDCMotor(7);
 BLDCDriver6PWM driver = BLDCDriver6PWM(A_PHASE_UH, A_PHASE_UL, A_PHASE_VH, A_PHASE_VL, A_PHASE_WH, A_PHASE_WL);
 LowsideCurrentSense currentSense = LowsideCurrentSense(0.003f, -64.0f / 7.0f, A_OP1_OUT, A_OP2_OUT, A_OP3_OUT);
 
+// ===== エンコーダ =====
 Encoder encoder = Encoder(A_HALL1, A_HALL2, 2048, A_HALL3);
-
 void doA() { encoder.handleA(); }
 void doB() { encoder.handleB(); }
 void doIndex() { encoder.handleIndex(); }
 
+// ===== コマンダ =====
 Commander command = Commander(Serial);
 void doTarget(char *cmd) { command.motion(&motor, cmd); }
 
 void setup()
 {
+    // ===== センサ =====
     encoder.init();
     encoder.enableInterrupts(doA, doB, doIndex);
-
     motor.linkSensor(&encoder);
 
+    // ===== ドライバ =====
     driver.voltage_power_supply = 16;
     driver.init();
     motor.linkDriver(&driver);
 
+    // ===== 電流検出 =====
     currentSense.linkDriver(&driver);
     currentSense.init();
     currentSense.skip_align = true;
     motor.linkCurrentSense(&currentSense);
 
+    // ===== 基本設定 =====
     motor.voltage_sensor_align = 1;
     motor.velocity_index_search = 3;
-    motor.voltage_limit = 6;
-    motor.velocity_limit = 1000;
 
+    // 🔥 電圧フルに使う
+    motor.voltage_limit = 15;   // 電源いっぱいまで使う
+
+    // 🔥 速度制御
     motor.controller = MotionControlType::velocity;
     motor.torque_controller = TorqueControlType::foc_current;
 
+    // 🔥 制限を全部開放気味に
+    motor.current_limit = 10;      // 要注意（様子見ながら）
+    motor.velocity_limit = 10000;  // 実質無制限
+
+    // ===== 電流PID =====
     motor.PID_current_q.P = motor.PID_current_d.P = 0.1;
     motor.PID_current_q.I = motor.PID_current_d.I = 10;
 
+    // ===== 速度PID（強め） =====
     motor.PID_velocity.P = 0.5;
-    motor.PID_velocity.I = 1;
-    motor.PID_velocity.output_ramp = 1000;
-    motor.LPF_velocity.Tf = 0.01;
+    motor.PID_velocity.I = 10;
+    motor.PID_velocity.output_ramp = 0;   // 加速制限なし
+    motor.LPF_velocity.Tf = 0.005;
 
-    motor.P_angle.P = 20;
-
+    // ===== 通信 =====
     Serial.begin(115200);
     motor.useMonitoring(Serial);
 
     motor.init();
     motor.initFOC();
-    command.add('T', doTarget, "target angle");
 
-    Serial.println(F("Motor ready."));
-    Serial.println(F("Set the target angle using serial terminal:"));
-    _delay(1000);
+    command.add('T', doTarget, "target velocity");
+
+    Serial.println("Motor ready.");
+    Serial.println("Send: T<velocity rad/s>");
+    delay(1000);
 }
 
 void loop()
 {
-    motor.move();
     motor.loopFOC();
+    motor.move();
     command.run();
 }
-#endif
